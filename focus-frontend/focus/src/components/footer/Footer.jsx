bb0467853e7e70c790cf5bb74951d7067fd32c3d
Utsab Mondal <utsabmondal19@gmail.com>
2025-08-16 19:45:39 -0400

    Added footer

---
+++ focus-frontend/focus/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer class='footer'>
+                <span>All rights reserved 2025 by Utsab Mondal</span>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
