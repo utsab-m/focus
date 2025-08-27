f33bcd96c929cebb60b5609bd3812b5c62a7f804
Utsab Mondal <utsabmondal19@gmail.com>
2025-08-27 08:55:52 -0400

    Added h1 tags to login page

--- focus-frontend/src/components/tasks/Task.jsx
+++ focus-frontend/src/components/tasks/Task.jsx
@@ -7,13 +7,14 @@ const Task = ({task}) => {
 
     function toggleTaskCompleted() {
         setCompleted(!task.completed);
-        updateTask(task.id, task);
+        task.completed = completed;
+        console.log(updateTask(task.id, task));
     }
 
     return (
         <div class="mt-1.5 mb-1.25 rounded-lg bg-blue-400 text-center flex items-center h-40 w-150">
             <div class="bg-black h-full w-1.5 hover:bg-white"></div>
-            <button class={`rounded-[50%] ${completed ? "bg-green-700" : "hover:bg-green-700"}`} onClick={toggleTaskCompleted}>
+            <button class={`rounded-[50%] ${completed ? "bg-green-700" : "bg-transparent hover:bg-green-700"}`} onClick={toggleTaskCompleted}>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Eo_circle_green_checkmark.svg/640px-Eo_circle_green_checkmark.svg.png" width="50" height="50"></img>
             </button>
             <div class="flex flex-col">
--- focus-frontend/src/pages/LoginPage.jsx
+++ focus-frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,14 @@
 const LoginPage = () => {
     return (
-        <div class="flex-col flex h-screen overflow-hidden bg:dark-gray-900">
-            <div class="bg:dark-gray-900">
-                <h1>Hi</h1>
+        <div class="flex-col flex h-screen justify-items-center items-center overflow-hidden dark:bg-gray-900">
+            <div class="text-white h-screen bg-dark-gray-900">
+                <div>
+                    <h1>Username</h1>
+                </div>
+                <div>
+                    <h1>Password</h1>
+                </div>
+                
             </div>
         </div>
     )
