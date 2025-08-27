2436d01646d9ad54f43fbc593d9ce9cb3024d802
Utsab Mondal <utsabmondal19@gmail.com>
2025-08-27 08:45:51 -0400

    Completed button works

--- focus-frontend/src/components/tasks/Task.jsx
+++ focus-frontend/src/components/tasks/Task.jsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
+import { updateTask } from "../../services/TaskService";
 
 const Task = ({task}) => {
 
-    const [completed, useCompleted] = useState(task.completed);
+    const [completed, setCompleted] = useState(task.completed);
 
     function toggleTaskCompleted() {
-        // completed = !completed;
-        // if (completed) {
-            
-        // }
-        alert("Completed")
+        setCompleted(!task.completed);
+        updateTask(task.id, task);
     }
 
     return (
-        <div class="mt-1.5 mb-1.25 rounded-lg sm:flex sm:items-center sm:justify-between bg-blue-400 text-center justify-center flex h-15 w-150">
-            <button class="rounded-[50%] hover:bg-green-700" onClick={toggleTaskCompleted}>
+        <div class="mt-1.5 mb-1.25 rounded-lg bg-blue-400 text-center flex items-center h-40 w-150">
+            <div class="bg-black h-full w-1.5 hover:bg-white"></div>
+            <button class={`rounded-[50%] ${completed ? "bg-green-700" : "hover:bg-green-700"}`} onClick={toggleTaskCompleted}>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Eo_circle_green_checkmark.svg/640px-Eo_circle_green_checkmark.svg.png" width="50" height="50"></img>
             </button>
-            <span>{task.name}</span>
-            
-            <span>{task.description}</span>
+            <div class="flex flex-col">
+                <span>{task.name}</span>
+                <span>{task.description}</span>
+            </div>
         </div>
     )
 };
