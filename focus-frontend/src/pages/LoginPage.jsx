c3f9ff30347451781b11a6360292cb6cb050ac1d
Utsab Mondal <utsabmondal19@gmail.com>
2025-09-03 09:01:57 -0400

    Added input validation for username

--- focus-frontend/src/pages/LoginPage.jsx
+++ focus-frontend/src/pages/LoginPage.jsx
@@ -10,6 +10,41 @@ const LoginPage = () => {
         password: ''
     });
 
+    function validUser(user) {
+        for (i = 0; i < user.length; i++) {
+            if (
+                !((user.charCodeAt(i) >= 48 && user.charCodeAt(i) <= 57) ||
+                (user.charCodeAt(i) >= 65 && user.charCodeAt(i) <= 90) ||
+                (user.charCodeAt(i) >= 97 && user.charCodeAt(i) <= 122))
+            ) return false;
+        }
+
+        return true;
+    }
+
+    function validateForm() {
+        let valid = true;
+        const errorCopy = {... errors};
+
+        let user = username.trim();
+
+        if (validUser(user)) {
+
+        } else {
+            valid = false;
+        }
+
+        let pass = password.trim();
+
+        if (validPass(pass)) {
+
+        } else {
+            valid = false;
+        }
+
+        return valid;
+    }
+
     return (
         <div class="flex-col flex h-screen justify-items-center items-center overflow-hidden dark:bg-gray-900">
             <div class="text-white h-screen bg-dark-gray-900">
