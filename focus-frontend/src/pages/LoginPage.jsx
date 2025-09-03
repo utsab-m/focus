31b865e1b433ca76216826b2cba804f6165f999f
Utsab Mondal <utsabmondal19@gmail.com>
2025-09-03 08:43:53 -0400

    Added input for user/pass

--- focus-frontend/src/pages/LoginPage.jsx
+++ focus-frontend/src/pages/LoginPage.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react"
+
 const LoginPage = () => {
+
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const [errors, setErrors] = useState({
+        username: '',
+        password: ''
+    });
+
     return (
         <div class="flex-col flex h-screen justify-items-center items-center overflow-hidden dark:bg-gray-900">
             <div class="text-white h-screen bg-dark-gray-900">
-                <div>
-                    <h1>Username</h1>
-                </div>
-                <div>
-                    <h1>Password</h1>
-                </div>
+                <form>
+                    <label for="username">Username: </label>
+                    <input class="bg-white text-black" type="text" id="username" name="username" required></input><br></br>
+
+                    <label for="password">Password: </label>
+                    <input class="bg-white text-black" type="password" id="password" name="password" required></input>
+                </form>
                 
             </div>
         </div>
