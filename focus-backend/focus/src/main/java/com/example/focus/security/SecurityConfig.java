7070cfbdd4c9b9842bb1b7e3890b76a8b9f62a76
Utsab Mondal <utsabmondal19@gmail.com>
2025-10-02 08:45:25 -0400

    Fixed versions

--- focus-backend/focus/src/main/java/com/example/focus/security/SecurityConfig.java
+++ focus-backend/focus/src/main/java/com/example/focus/security/SecurityConfig.java
@@ -1,25 +1,34 @@
 package com.example.focus.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/tasks/**").authenticated()
                         .anyRequest().authenticated()
-                );
+                )
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+                        .sessionCreatePolicy(SessionCreatePolicy.STATELESS))
+                .addFilterBefore();
         return http.build();
+    }
+
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+
     }
 }
